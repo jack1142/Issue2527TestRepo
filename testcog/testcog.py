3a6b93a3ad6493e0bc4b74c2e2b2c9ae8c5490f3
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-01-19 19:47:07 +0100

    blah

--- testcog/testcog.py
+++ testcog/testcog.py
@@ -13,7 +13,7 @@ class TestCog(commands.Cog):
         self.bot = bot
 
     def z(self):
-        raise RuntimeError("XD")
+        raise RuntimeError("Something went horribly wrong.")
 
     def y(self):
         self.z()
@@ -31,12 +31,12 @@ class TestCog(commands.Cog):
     @commands.command()
     async def testcommand(self, ctx, x: int, y: int):
         """Settings for voice tools"""
-        result = x + y
+        raise RuntimeError("Something went horribly wrong.")
         await ctx.send(f"Result of {x} + {y} is {result}")
 
     @commands.is_owner()
     @commands.command()
     async def anothercommand(self, ctx, x: int, y: int):
         """Settings for voice tools"""
-        result = x * y
+        raise RuntimeError("Something went horribly wrong.")
         await ctx.send(f"Result of {x} * {y} is {result}")
