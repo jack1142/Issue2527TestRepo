e18422920411f3128d58cb7ec62f9249bac9f892
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-01-19 18:38:35 +0100

    testcog

---
+++ testcog/testcog.py
@@ -0,0 +1,42 @@
+import asyncio
+import logging
+
+from redbot.core import commands
+
+log = logging.getLogger("red.testcog")
+
+
+class TestCog(commands.Cog):
+    """Some dummy cog for Downloader testing."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def z(self):
+        raise RuntimeError("XD")
+
+    def y(self):
+        self.z()
+
+    async def x(self):
+        self.y()
+
+    def create_task_x(self, *args):
+        asyncio.create_task(self.x())
+
+    async def initialize(self):
+        self.create_task_x()
+
+    @commands.is_owner()
+    @commands.command()
+    async def testcommand(self, ctx, x: int, y: int):
+        """Settings for voice tools"""
+        result = x + y
+        await ctx.send(f"Result of {x} + {y} is {result}")
+
+    @commands.is_owner()
+    @commands.command()
+    async def anothercommand(self, ctx, x: int, y: int):
+        """Settings for voice tools"""
+        result = x * y
+        await ctx.send(f"Result of {x} * {y} is {result}")
